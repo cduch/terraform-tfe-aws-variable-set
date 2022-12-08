b9a38cfd8e6c6d9ddc882a70d0c202a21e8b75b3
Carsten Duch <carsten@hashicorp.com>
2022-12-08 13:10:44 +0100

    less go 5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # terraform-tfe-aws-variable-set
-create a variable set with AWS credentials
+create a variable set with AWS credentials dynamically.
+
+## Security risk
+This is just a workaround, don't use in production.
--- main.tf
+++ main.tf
@@ -7,9 +7,6 @@ data "external" "env" {
   program = ["${path.module}/env.sh"]
 }
 
-output "env" {
-  value = data.external.env.result
-}
 
 resource "tfe_variable_set" "variableset" {
   name         = var.variableset_name
@@ -23,6 +20,36 @@ resource "tfe_variable" "awsaccesskeyid" {
   value           = data.external.env.result["AWS_ACCESS_KEY_ID"]
   category        = "env"
   description     = "AWS ACCESS KEY ID"
+  sensitive       = false
+  variable_set_id = tfe_variable_set.variableset.id
+}
+
+
+resource "tfe_variable" "awssecretaccesskey" {
+  key             = "AWS_SECRET_ACCESS_KEY"
+  value           = data.external.env.result["AWS_SECRET_ACCESS_KEY"]
+  category        = "env"
+  description     = "AWS_SECRET_ACCESS_KEY"
   sensitive       = true
+  variable_set_id = tfe_variable_set.variableset.id
+}
+
+
+resource "tfe_variable" "awssessiontoken" {
+  key             = "AWS_SESSION_TOKEN"
+  value           = data.external.env.result["AWS_SESSION_TOKEN"]
+  category        = "env"
+  description     = "AWS SESSION TOKEN"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.variableset.id
+}
+
+
+resource "tfe_variable" "awssessionexpiration" {
+  key             = "AWS_SESSION_EXPIRATION"
+  value           = data.external.env.result["AWS_SESSION_EXPIRATION"]
+  category        = "env"
+  description     = "AWS SESSION Expiration"
+  sensitive       = false
   variable_set_id = tfe_variable_set.variableset.id
 }
